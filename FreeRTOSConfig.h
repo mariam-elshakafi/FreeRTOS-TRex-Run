9278e6f3646b40a316d50e890f552c992f052f33
mariam-elshakafi <mariam.elshakafi@gmail.com>
2020-06-01 21:54:12 +0200

    Replaced tabs with spaces

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -40,34 +40,34 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION		0
-#define configUSE_IDLE_HOOK			0
-#define configUSE_TICK_HOOK			0
-#define configCPU_CLOCK_HZ			( ( unsigned long ) 16000000 )
-#define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES		( 5 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 1800 ) )
-#define configMAX_TASK_NAME_LEN		( 16 )
-#define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		1
-#define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES			    1
+#define configUSE_PREEMPTION        0
+#define configUSE_IDLE_HOOK         0
+#define configUSE_TICK_HOOK         0
+#define configCPU_CLOCK_HZ          ( ( unsigned long ) 16000000 )
+#define configTICK_RATE_HZ          ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES        ( 5 )
+#define configMINIMAL_STACK_SIZE    ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE       ( ( size_t ) ( 1800 ) )
+#define configMAX_TASK_NAME_LEN     ( 16 )
+#define configUSE_TRACE_FACILITY    0
+#define configUSE_16_BIT_TICKS      1
+#define configIDLE_SHOULD_YIELD     1
+#define configUSE_MUTEXES           1
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		0
+#define configUSE_CO_ROUTINES             0
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet	   	0
-#define INCLUDE_uxTaskPriorityGet	  	0
-#define INCLUDE_vTaskDelete				    1
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend			    0
-#define INCLUDE_vTaskDelayUntil			  0
-#define INCLUDE_vTaskDelay				    1
+#define INCLUDE_vTaskPrioritySet        0
+#define INCLUDE_uxTaskPriorityGet       0
+#define INCLUDE_vTaskDelete             1
+#define INCLUDE_vTaskCleanUpResources   0
+#define INCLUDE_vTaskSuspend            0
+#define INCLUDE_vTaskDelayUntil         0
+#define INCLUDE_vTaskDelay              1
 
 /* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
 (lowest) to 0 (1?) (highest). */
